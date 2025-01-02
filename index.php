e8a55209fcfdd94120b3beaaeeaa317dff895e3a
alenta <alenta@gmail.com>
2025-01-02 20:07:18 +0530

    index page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concert Management System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        Concert Management System
+    </header>
+    <div class="nav">
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+    </div>
+    <div class="welcome">
+        <h1>Welcome to the Concert Management System</h1>
+        <p>To get your tickets ready ..... Register now and get started!</p>
+    </div>
+</body>
+</html>
